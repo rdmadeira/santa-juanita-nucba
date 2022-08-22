a6432fd8b5f400bc733449b1441f00b00090a17c
rdmadeira <rdmadeira2@gmail.com>
2022-08-22 17:43:31 -0300

    insertando productos

--- js/user.js
+++ js/user.js
@@ -50,6 +50,34 @@ const getProductos = async () => {
 }
 async function asignProductos () {
     const productos = await getProductos();
-    console.log(productos);
+    const productsCtnEl =  document.getElementById('products-ctn');
+
+    await productos && loadProducts(productos.todoslosproductos);
+
+    function loadProducts(array) {
+        
+        const loadCtn = document.getElementById('loading-ctn');
+        loadCtn.style.display = 'none';
+        array.forEach(item => {
+            let newCtn = document.createElement('div');
+            let newH3 = document.createElement('h3');
+            let newP1 = document.createElement('p');
+            let newP2 = document.createElement('p');
+            let newImg = document.createElement('img');
+    
+            newCtn.classList.add('product-ctn');
+            newH3.innerText = item.name;
+            newP1.innerText = item.description;
+            if (item.type = 'vela') {
+                newP2.innerText = item.price;
+                
+            }
+            newImg.src = item.img;
+    
+            newCtn.append(newImg, newH3, newP1);
+            productsCtnEl.appendChild(newCtn);
+        });
+    }
+
 }
-window.addEventListener('load', () => asignProductos())
+window.addEventListener('load', () => asignProductos());
